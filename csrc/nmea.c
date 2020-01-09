dbefa9f04885f6d50a658d103fab09837814cc63
Zeryntth <dev@zerynth.com>
2020-01-09 09:40:39 +0000

    r2.3.3-base

---
+++ csrc/nmea.c
@@ -0,0 +1,310 @@
+#define ZERYNTH_PRINTF
+#include "zerynth.h"
+
+#define NMEA_DEBUG 0
+
+#if NMEA_DEBUG
+#define DEBUG(...) printf(__VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+
+double vatof(uint8_t *s, int32_t len, err_t *err);
+
+C_NATIVE(_nmea_readline){
+    NATIVE_UNWARN();
+    int32_t ser;
+    uint8_t *buf;
+    int32_t buflen;
+    int32_t timeout=5000;
+
+    if (parse_py_args("isi", nargs, args, &ser, &buf, &buflen, &timeout) != 3) {
+        *res = MAKE_NONE();
+        return ERR_TYPE_EXC;
+    }
+    ser&=0xFF;
+
+    int bytes=0,dollar=0;
+    uint8_t *ptr = buf;
+    uint8_t *end = NULL;
+    uint32_t crc=0,lcrc=0;
+
+    RELEASE_GIL();
+    uint32_t tstart = vosMillis();
+    while (bytes < buflen-1) {
+        if (timeout > 0) {
+            if ((vosMillis() - tstart) > timeout) {
+                buf++;
+                *buf=0;
+                ACQUIRE_GIL();
+                *res = MAKE_NONE();
+                return ERR_TIMEOUT_EXC;
+            }
+            if (vhalSerialAvailable(ser) > 0) {
+                vhalSerialRead(ser, ptr, 1);
+                tstart = vosMillis();
+            } else {
+                vosThSleep(TIME_U(50, MILLIS));
+                continue;
+            }
+        } else {
+            vhalSerialRead(ser, ptr, 1);
+        }
+        bytes++;
+        if (*ptr == '$'){
+            // restart
+            ptr = buf;
+            *ptr = '$';
+            bytes = 1;
+            dollar = 1;
+        }
+        else if (*ptr == '\r' || *ptr == '\n') {
+            if (dollar)
+                break;
+            // restart
+            ptr = buf;
+        }
+        ptr++;
+    }
+    ACQUIRE_GIL();
+    *ptr = 0;
+
+    // checks
+    if (*buf != '$'){
+        //invalid line
+        *res = PSMALLINT_NEW(-1);
+    }
+    else {
+        for(ptr=buf+1; ptr<buf+bytes; ++ptr) {
+            if (*ptr=='*') {
+                end=ptr;
+                break;
+            }
+            crc = crc^*ptr;
+        }
+    }
+    if (!end){
+        //invalid line
+        *res = PSMALLINT_NEW(-2);
+    }
+    else {
+        //read checksum
+        uint8_t ch=*(end+1);
+        if (ch>='0'&&ch<='9') lcrc += (ch-'0')*16;
+        else if(ch>='A'&&ch<='F') lcrc += (ch-'A'+10)*16;
+        else lcrc=-1;
+        ch=*(end+2);
+        if (ch>='0'&&ch<='9') lcrc += (ch-'0');
+        else if(ch>='A'&&ch<='F') lcrc += (ch-'A'+10);
+        else lcrc=-1;
+
+        if(crc!=lcrc) {
+            //bad checksum
+            *res = PSMALLINT_NEW(-3);
+        } else {
+            *res = PSMALLINT_NEW((end-buf));
+        }
+    }
+    
+    DEBUG("> %i %s\n",bytes,buf);
+    return ERR_OK;
+}
+
+
+typedef struct _splits {
+    uint8_t* start;
+    int len;
+} NmeaSplit;
+NmeaSplit nmea_flds[20];
+
+int _do_split(uint8_t *line, int len, NmeaSplit *flds, int fldlen) {
+    int i,j=0;
+    uint8_t *start = line;
+    memset(flds,0,sizeof(NmeaSplit)*fldlen);
+    for (i=0;i<len;i++) {
+        if (j>=fldlen) break;
+        if (line[i]==',') {
+            flds[j].start = start;
+            flds[j].len = (line+i)-start;
+            start = line+i+1;
+            j++;
+        }
+    }
+    return j; // num of fields (not counting first cmd)
+}
+
+int _nmea_set_time(NmeaSplit *tm, NmeaSplit *dt, int *yy, int *mt, int *dd, int* hh, int* mm, int *ss,int*uu){
+    int err;
+    //parse date: ddmmyy or ddmmyyyy
+    *yy = vatoi(dt->start+4,dt->len-3,10,&err);
+
+    if (*yy>=100) {
+    } else if(*yy>=80) {
+        *yy=1900+*yy;
+    }else { 
+        *yy=2000+*yy;
+    }
+    *mt = vatoi(dt->start+2,2,10,&err); 
+    *dd = vatoi(dt->start,2,10,&err); 
+
+    //parse time
+    *hh = vatoi(tm->start,2,10,&err);
+    *mm = vatoi(tm->start+2,2,10,&err);
+    *ss = vatoi(tm->start+4,2,10,&err);
+    *uu = vatoi(tm->start+7,tm->len-6,10,&err);
+    
+    return 1;
+}
+
+
+int _nmea_set_pos(NmeaSplit *latf,NmeaSplit *latpf,NmeaSplit *lonf, NmeaSplit *lonpf, double *lat, double *lon){
+    int err;
+	int i;
+	for (i=latf->len; i>0; --i)
+		if (latf->start[i] == '.') {
+			i -= 2;
+			break;
+		}
+    *lat = vatof(latf->start+i,latf->len-i,&err) / 60.0 + vatoi(latf->start,i,10,&err);
+	for (i=lonf->len; i>0; --i)
+		if (lonf->start[i] == '.') {
+			i -= 2;
+			break;
+		}
+    *lon = vatof(lonf->start+i,lonf->len-i,&err) / 60.0 + vatoi(lonf->start,i,10,&err);
+    if(*(latpf->start)=='S') *lat = -1.0*(*lat);
+    if(*(lonpf->start)=='W') *lon = -1.0*(*lon);
+    return 1;
+}
+
+int _nmea_set_spd(NmeaSplit *spdf, double *spd){
+    int err;
+    *spd = vatof(spdf->start,spdf->len,&err)*1.852; //knots to km/h
+    return 1;
+}
+
+int _nmea_set_cog(NmeaSplit *cogf, double *cog){
+    int err;
+    *cog = vatof(cogf->start,cogf->len,&err);
+    return 1;
+}
+
+C_NATIVE(_nmea_parseline){
+    NATIVE_UNWARN();
+    uint8_t *buf;
+    int32_t buflen;
+    int32_t len;
+
+    if (nargs != 4 || parse_py_args("si", 2, args, &buf, &buflen, &len) != 2)
+        return ERR_TYPE_EXC;
+
+    DEBUG("parse buflen=%d len=%d\n",buflen,len);
+
+    uint8_t *end = buf+len;
+    uint8_t *start = buf;
+    uint8_t *line = start;
+    int ln = end-start;
+    uint8_t *cmd = line+3;
+    int fldn;
+    int has_time=0,gcmd=0,has_fix=0;
+    int yy,mt,dd,hh,mm,ss,uu,nfix,err=0;
+    double lat,lon,alt,spd,cog,hdop,vdop,pdop;
+    PList *tm = args[2];
+    PList *fix = args[3];
+    NmeaSplit *flds=nmea_flds;
+
+    if (ln<8) {
+        *res = PSMALLINT_NEW(0);
+        return ERR_OK;
+    }
+
+    //vosThSleep(TIME_U(10,MILLIS));
+    RELEASE_GIL();
+    
+    if(memcmp(cmd,"RMC",3)==0) {
+        fldn = _do_split(line,ln,flds,20);
+        DEBUG("rmc split %i\n",fldn);
+        if (fldn<12) {
+            gcmd = 0;
+        } else {
+            if (*flds[2].start!='A') {
+                //no fix 
+            } else {
+                gcmd = 1;
+                has_time = _nmea_set_time(&flds[1],&flds[9],&yy,&mt,&dd,&hh,&mm,&ss,&uu);
+                has_fix++;
+                _nmea_set_pos(&flds[3],&flds[4],&flds[5],&flds[6],&lat,&lon);
+                _nmea_set_spd(&flds[7],&spd);
+                _nmea_set_cog(&flds[8],&cog);
+            }
+        }
+    } else if (memcmp(cmd,"GGA",3)==0) {
+        fldn = _do_split(line,ln,flds,20);
+        DEBUG("gga split %i\n",fldn);
+        if (fldn<14) {
+            gcmd = 0;
+        } else {
+            if (*flds[6].start=='0') {
+                gcmd=0;
+            } else {
+                gcmd=2;
+                has_fix++;
+                hdop = vatof(flds[8].start,flds[8].len,&err);
+                alt = vatof(flds[9].start,flds[9].len,&err);
+                nfix = vatoi(flds[7].start,flds[7].len,10,&err);
+            }
+        }
+    } else if (memcmp(cmd,"GSA",3)==0) {
+        fldn = _do_split(line,ln,flds,20);
+        DEBUG("gsa split %i\n",fldn);
+        if (fldn<18) {
+            gcmd = 0;
+        } else {
+            if (*flds[2].start!='3') {
+                gcmd=0;
+            } else {
+                gcmd=3;
+                has_fix++;
+                hdop = vatof(flds[16].start,flds[17].len,&err);
+                vdop = vatof(flds[17].start,flds[17].len,&err);
+                pdop = vatof(flds[15].start,flds[15].len,&err);
+            }
+        }
+    } else {
+        gcmd = 0;
+    }
+
+    ACQUIRE_GIL();
+
+    if (has_time){
+        PLIST_SET_ITEM(tm,0,PSMALLINT_NEW(yy));
+        PLIST_SET_ITEM(tm,1,PSMALLINT_NEW(mt));
+        PLIST_SET_ITEM(tm,2,PSMALLINT_NEW(dd));
+        PLIST_SET_ITEM(tm,3,PSMALLINT_NEW(hh));
+        PLIST_SET_ITEM(tm,4,PSMALLINT_NEW(mm));
+        PLIST_SET_ITEM(tm,5,PSMALLINT_NEW(ss));
+        PLIST_SET_ITEM(tm,6,PSMALLINT_NEW(uu));
+        DEBUG("has_time %i %i %i %i %i %i %i\n",yy,mt,dd,hh,mm,ss,uu);
+    }
+
+    if(has_fix){
+        if (gcmd==1) {
+            PLIST_SET_ITEM(fix,0,pfloat_new(lat));
+            PLIST_SET_ITEM(fix,1,pfloat_new(lon));
+            PLIST_SET_ITEM(fix,3,pfloat_new(spd));
+            PLIST_SET_ITEM(fix,4,pfloat_new(cog));
+        } else if(gcmd==2)   {
+            PLIST_SET_ITEM(fix,6,pfloat_new(hdop));
+            PLIST_SET_ITEM(fix,2,pfloat_new(alt));
+            PLIST_SET_ITEM(fix,5,PSMALLINT_NEW(nfix));
+        } else if(gcmd==3){
+            PLIST_SET_ITEM(fix,6,pfloat_new(hdop));
+            PLIST_SET_ITEM(fix,7,pfloat_new(vdop));
+            PLIST_SET_ITEM(fix,8,pfloat_new(pdop));
+        }
+    }
+
+    gcmd=(has_time<<2)+gcmd;
+    *res = PSMALLINT_NEW(gcmd);
+    return ERR_OK;
+}
---
+++ nmea.py
@@ -0,0 +1,213 @@
+"""
+.. module:: nmea
+
+***********
+NMEA Module
+***********
+
+This module implements a Zerynth driver and parser for a generic NMEA GNSS receiver.
+
+
+The following functionalities are implemented:
+
+    * retrieve the current location fix if present
+    * retrieve the current UTC time
+
+The driver supports reading NMEA sentences from a serial port only.
+
+Location fixes are obtained by parsing NMEA sentences of type RMC and GGA, and optionally GSA.
+Obtaining a fix or UTC time are thread safe operations.
+
+    """
+
+import streams
+import threading
+
+@c_native("_nmea_readline",["csrc/nmea.c"])
+def readline(serial,buffer,timeout=5000):
+    """
+.. method:: readline(serial,buffer,timeout=5000)
+
+    Wait for a full NMEA sentence from the specified *serial* interface and copy it
+    to the specified *buffer* (bytearray), with optional *timeout* (in milliseconds).
+
+    Returns the length of the NMEA sentence or a negative error code:
+
+    * *-1*, if the line does not start with NMEA header (missing :samp:`'$'`)
+    * *-2*, if the line has incomplete NMEA sentence (missing  :samp:`'*'`)
+    * *-3*, if the line has invalid or mismatching NMEA checksum
+
+    """
+    pass
+
+@c_native("_nmea_parseline",["csrc/nmea.c"])
+def parseline(buffer,length,tm,fix):
+    """
+.. method:: parseline(buffer,length,tm,fix)
+
+    Parse the content of the specified line *buffer* (bytearray) up to *length* bytes
+    and fill the two sequences *tm* and *fix* with date/time and fix data if available.
+
+    Returned value is *0* if the line does not have valid data, or a combination (sum) of:
+    
+    * *4*, if the *tm* sequence (7 items) has been filled (from RMC sentence)
+    * *1, 2 or 3*, if the *fix* sequence (9 items) has been filled (from RMC, GGA or GSA respectively)
+
+    """
+    pass
+
+class NMEA_Receiver():
+    """
+
+.. class:: NMEA_Receiver()
+
+    This class is meant to be used as a base class to provide a uniform interface for GNSS receivers.
+
+    Instances of this class are fed with NMEA sentences using the :any:`parse` method and can be
+    queried for UTC time and location data.
+
+    """
+
+    def __init__(self):
+        self.debug = False
+        self._tm = [0,0,0,0,0,0,0]
+        self._lat = 0
+        self._lon = 0
+        self._spd = 0
+        self._cog = 0
+        self._nsat = 0
+        self._hdop = 0
+        self._vdop = 0
+        self._pdop = 0
+        self._alt  = 0
+        self._cfix = [None]*10
+        self._time = None
+        self._fix = None
+        self._rmc = False
+        self._gga = False
+        self._gsa = False
+        self._utc = False
+        self._lock = threading.Lock()
+
+
+    def print_d(self,*args):
+        if self.debug:
+            print('>', *args)
+
+
+    def fix(self):
+        """
+.. method:: fix()
+
+        Return the current fix or *None* if not available.
+        A fix is a tuple with the following elements:
+
+            * latitude in decimal format (-89.9999 - 89.9999)
+            * longitude in decimal format (-179.9999 - 179.9999)
+            * altitude in meters
+            * speed in Km/h
+            * course over ground as degrees from true north
+            * number of satellites for this fix
+            * horizontal dilution of precision (0.5 - 99.9)
+            * vertical dilution of precision (0.5 - 99.9)
+            * positional dilution of precision (0.5 - 99.9)
+            * UTC time as a tuple (yyyy,MM,dd,hh,mm,ss,microseconds)
+
+        """
+        self._lock.acquire()
+        r = self._fix
+        self._fix = None
+        self._lock.release()
+        return r
+
+    def has_fix(self):
+        """
+.. method:: has_fix()
+    
+        Return *True* if a fix is available
+
+        """
+        self._lock.acquire()
+        r = self._fix
+        self._lock.release()
+        return r is not None
+
+    def utc(self):
+        """
+.. method:: utc()
+
+        Return the current UTC time or *None* if not available.
+        A UTC time is a tuple of (yyyy,MM,dd,hh,mm,ss,microseconds).
+
+        UTC time can be wrong if no fix has ever been obtained.
+        """
+        r= None
+        self._lock.acquire()
+        if self._time:
+            r = self._time
+            self._time = None
+        self._lock.release()
+        return r
+
+    def has_utc(self):
+        """
+.. method:: has_utc()
+    
+        Return *True* if a UTC time is available
+
+        """
+        self._lock.acquire()
+        r = self._time
+        self._lock.release()
+        return r is not None
+
+
+    ##################### Private
+
+    def parse(self,buffer,count):
+        """
+.. method:: parse(buffer,count)
+    
+        Parse *count* bytes from the specified *buffer* (bytearray) and updates internal state
+        from valid NMEA sentences found.
+
+        """
+        r = parseline(buffer,count,self._tm,self._cfix)
+        #print("R",r)
+        if r&4:
+            # we have time
+            self._lock.acquire()
+            self._time = (self._tm[0],self._tm[1],self._tm[2],self._tm[3],self._tm[4],self._tm[5],self._tm[6])
+            self._lock.release()
+        r=r&3
+        if r==1:
+            self._rmc=True
+        elif r==2:
+            self._gga=True
+        elif r==3:
+            self._gsa=True
+
+        #print(self._rmc,self._gga,self._gsa,r)
+        if self._rmc and self._gga:
+            # we have a fix
+            if not self._gsa:
+                # optional data VDOP, PDOP
+                self._cfix[7] = None
+                self._cfix[8] = None
+            self._lock.acquire()
+            self._fix = (
+                    self._cfix[0],
+                    self._cfix[1],
+                    self._cfix[2],
+                    self._cfix[3],
+                    self._cfix[4],
+                    self._cfix[5],
+                    self._cfix[6],
+                    self._cfix[7],
+                    self._cfix[8],
+                    (self._tm[0],self._tm[1],self._tm[2],self._tm[3],self._tm[4],self._tm[5],self._tm[6])
+                    )
+            self._rmc = False
+            self._gga = False
+            self._gsa = False
+            self._lock.release()
